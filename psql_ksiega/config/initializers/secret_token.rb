3319125cee0fdbe2a112ad5f8463ffc80a707d27
tsott <tsott@sigma.inf.ug.edu.pl>
2012-12-16 16:54:54 +0100

    wersja w psql na heroku

---
+++ psql_ksiega/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsqlKsiega::Application.config.secret_token = '8476b86192254313e9a10fb107f57e65a32ef631c03e9d717ae8af10e2f4cb073b733a277c3307f2a71371d78f056a3ace356d198afd78bd208749c3ffb1ce91'
